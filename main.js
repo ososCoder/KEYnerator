585580cc1109d19048ecdc75fea17b75dd011724
oscar_ousinde <oscar.ousinde@hotmail.com>
2022-12-04 11:37:20 +0100

    Starting JS

--- main.js
+++ main.js
@@ -1,1 +1,16 @@
 'use strict';
+
+//SELECCIÓN DE ELEMENTOS/////////////////////////////////////////////////
+
+//inputs
+const radios = document.querySelectorAll('input[type="radio"]');
+const checks = document.querySelectorAll('input[type="checkbox"]');
+const range = document.querySelector('input[type="range"]');
+
+//buttons
+const generatePass = document.getElementsByClassName('generatePass');
+const copyPass = document.getElementsByClassName('copyPass');
+
+//CHECK DE ELEMENTOS////////////////////////////////////////////////////
+for (const radio of radios) {
+}
